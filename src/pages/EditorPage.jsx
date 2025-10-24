571d7cf6a84934a31bcf40040a9a083e660aeea2
Devanshu Prakash <devanshuprakash2301@gmail.com>
2025-10-24 18:35:01 +0530

    editor page

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -1,7 +1,21 @@
-import React,{useState} from 'react'
-
+import React,{useEffect, useRef, useState} from 'react'
+import Client from '../components/Client'
+import Editor from '../components/Editor'
+// import { initSocket } from '../socket';
 function EditorPage() {
-  const [clients,setClients] = useState([{}])
+//   const scoketRef = useRef(null);
+//   useEffect(()=>{
+//     const initSocket = async()=>{
+//       scoketRef.current = await  initSocket();
+//     };
+//     init();
+//   },[]);
+
+  const [clients,setClients] = useState([
+    {socketId:1,username:'Rake'},
+    {socketId:2,username:'deva'},
+    {socketId:3,username:'Rakesh kask'} 
+  ])
   return (
     <div className='mainWrap'>
       <div className="aside">
@@ -12,11 +26,27 @@ function EditorPage() {
             alt ='logo' />
           </div>
           <h3>Connected</h3>
-          <div className="clientList"></div>
+          <div className="clientList">
+              {
+               clients.map(client => {
+                    return (
+                      <Client
+                        key={client.socketId}
+                        username={client.username}
+                      />
+                    );
+                  })
+              }
+          </div>
         </div>
+        <button className='btn copyBtn'>Copy Room Id</button>
+        <button className='btn leaveBtn'>Leave</button>
+      </div>
+      <div className="editorWarp">
+        <Editor/>
       </div>
     </div>
-  )
-}
+  )}
+
 
 export default EditorPage;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,16 @@
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
+// const express = require('express');
+// const app = express();
+// const server = require('http').createServer(app);
+// const {Server} = require('socket.io');
+// const io = new Server(server);
+
+// // event trigger when a client connects
+// io.on('connection',(socket)=>{
+//     console.log('a user connected',socket.id);
+//   });
+
+// const PORT= process.env.Port || 4000;
+// server.listen(PORT,()=>{
+//   console.log(`server is running on port ${PORT}`);
+// });   
+
